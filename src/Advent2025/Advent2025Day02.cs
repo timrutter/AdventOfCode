aa230d85d6625fe4afd2bd328adedae833fa0c6a
Tim Rutter <tim.rutter@devsectr.com>
2025-12-03 09:44:11 +0000

    2025 days 1 to 3

--- src/Advent2025/Advent2025Day02.cs
+++ src/Advent2025/Advent2025Day02.cs
@@ -1,20 +1,85 @@
-﻿namespace AdventOfCode.Advent2025;
+﻿using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Helpers;
+
+namespace AdventOfCode.Advent2025;
 
 public class Advent2025Day02 : Solution
 {
     public Advent2025Day02()
     {
-        Answer1 = null;
-        Answer2 = null;
+        Answer1 = 13919717792;
+        Answer2 = 14582313461;
     }
 
     public override object ExecutePart1()
     {
-        return int.MaxValue;
+        var arr = DataFile.ReadAll<string>()[0].Split(",");
+        long total = 0;
+        foreach (var range in arr)
+        {
+            var bits = range.SplitToType<long>("-").ToList();
+            var start = bits[0];
+            var end = bits[1];
+            for (var i = start; i <= end; i++)
+            {
+                if (IsWrong(i))
+                {
+                    total += i;
+                }
+            }
+
+        }
+        return total;
     }
 
+    public static bool IsWrong(long i)
+    {
+        var s = i.ToString();
+        if (s.Length % 2 == 1) return false;
+        return s[..(s.Length / 2)] == s[(s.Length / 2)..];
+    }
+
+    public static bool IsWrong2(long i)
+    {
+        var s = i.ToString();
+        for (var div = 2; div <= s.Length; div++)
+            if (s.Length % div == 0)
+            {
+                var bit = s[..(s.Length / div)];
+                var step = s.Length / div;
+                var equal = true;
+                for(var pos = step; pos < s.Length ; pos+=step)
+                    if (bit != s[pos..(pos + step)])
+                    {
+                        equal = false;
+                        break;
+                    }
+
+                if (equal) return true;
+            }
+
+        return false;
+
+    }
     public override object ExecutePart2()
     {
-        return int.MaxValue;
+        var arr = DataFile.ReadAll<string>()[0].Split(",");
+        long total = 0;
+        foreach (var range in arr)
+        {
+            var bits = range.SplitToType<long>("-").ToList();
+            var start = bits[0];
+            var end = bits[1];
+            for (var i = start; i <= end; i++)
+            {
+                if (IsWrong2(i))
+                {
+                    total += i;
+                }
+            }
+
+        }
+        return total;
     }
 }
--- src/Advent2025/Advent2025Day03.cs
+++ src/Advent2025/Advent2025Day03.cs
@@ -1,20 +1,85 @@
-﻿namespace AdventOfCode.Advent2025;
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AdventOfCode.Helpers;
+
+namespace AdventOfCode.Advent2025;
 
 public class Advent2025Day03 : Solution
 {
     public Advent2025Day03()
     {
-        Answer1 = null;
-        Answer2 = null;
+        Answer1 = 17196L;
+        Answer2 = 171039099596062L;
     }
 
     public override object ExecutePart1()
     {
-        return int.MaxValue;
+        var arr = DataFile.ReadAll<string>();
+        return arr.Sum(Get2Max);
     }
 
     public override object ExecutePart2()
     {
-        return int.MaxValue;
+        var arr = DataFile.ReadAll<string>();
+        long sum = 0;
+
+        Parallel.ForEach(arr, line =>
+        {
+            var max = GetMax(line, 12);
+            Console.WriteLine(max);
+            Interlocked.Add(ref sum, max);
+        });
+        return sum;
+    }
+
+    private static long Get2Max(string line)
+    { 
+        var max = 0;
+        for (var j = 0; j < line.Length - 1; j++)
+        {
+            for (var k = j + 1; k < line.Length; k++)
+            {
+                var num = int.Parse($"{line[j]}{line[k]}");
+                max = Math.Max(num, max);
+            }
+        }
+
+        return max;
+    }
+
+    private static readonly ConcurrentDictionary<string, long> Seen = new();
+
+    public static long GetMax(string line, int length)
+    {
+        if (Seen.TryGetValue($"{line}_{length}", out var m1))
+            return m1;
+        long max = 0;
+        if (line.Length == length)
+        {
+            Seen[$"{line}_{length}" ] = long.Parse(line);
+            return Seen[$"{line}_{length}" ];
+        }
+        if (length == 1)
+        { 
+            Seen[$"{line}_{length}" ] =  line.Select(c => c - '0').Max();
+            return Seen[$"{line}_{length}" ];
+        }
+        
+        for (var i = 0; i <= line.Length - length; i++)
+        {
+            for (var j = i + 1; j <= line.Length - (length - 1); j++)
+            {
+                var s = line[j..];
+                var submax = GetMax(s, length - 1);
+                var s2 = $"{line[i]}{submax}";
+                max = Math.Max(max, long.Parse( s2));
+            }
+        }
+
+        Seen[$"{line}_{length}" ] = max;
+        return max;
     }
 }
