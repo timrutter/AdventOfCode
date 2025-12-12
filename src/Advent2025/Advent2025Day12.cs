1b853254c077a6654d33bf5648445200a783dcec
Tim Rutter <tim.rutter@devsectr.com>
2025-12-12 08:59:35 +0000

    Added some notes

--- src/Advent2025/Advent2025Day12.cs
+++ src/Advent2025/Advent2025Day12.cs
@@ -9,33 +9,33 @@ public class Advent2025Day12 : Solution
 {
     public Advent2025Day12()
     {
-        Answer1 = null;
+        Answer1 = 555;
         Answer2 = null;
     }
 
     public override object ExecutePart1()
     {
         var lines = DataFile.ReadAll<string>();
-        var lineNo = 0;
-
         var count = 0;
 
-        foreach (var region in lines.Skip(lineNo))
+        // this is pure estimation! Looking at each shape, the min loss per shape due to tessilation as a large block of
+        // the same shape is as follows
+        var losses = new List<double> { 0.5, 2, 1, 1, 0, 0 };
+        foreach (var region in lines.Skip(30))
         {
-            var losses = new List<double> { 0.5, 2, 1, 1, 0, 0 };
             var bits = region.Split(": ").ToList();
             var xy = bits[0].SplitToType<int>().ToList();
             var presentsToFit = bits[1].SplitToType<int>(" ").ToList();
             var totalPresentArea = presentsToFit.Select((p, i) => p * (9 - losses[i])).Sum();
             var area = xy[0] * xy[1];
-            if (totalPresentArea > area)
-            {
-                Console.WriteLine("TOO BIG");
-                continue;
-            }
+            // but whenever its not big enough the area - totalPresentArea < -400 and whenever its big enough its > 150
+            // i.e its never very close to fitting, only easily big enough or nowhere near big enough (carefully engineering to be the case?)
+            // so can only conclude that the loses are the biggest significant statistical factor therefore
+            // don't need to consider the arrangements at all.
+            if (totalPresentArea > area) continue;
 
-            Console.WriteLine(
-                $"{xy[0]}x{xy[1]} {string.Join('|', presentsToFit)} area:{area} presentArea {totalPresentArea}");
+            //Console.WriteLine(
+            //    $"{xy[0]}x{xy[1]} {string.Join('|', presentsToFit)} area:{area} presentArea {totalPresentArea}");
             count++;
         }
         return count;
